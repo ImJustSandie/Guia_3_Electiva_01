4d77e6e60b6623ca44eee5506db754c7caaf2243
SandovalAlejandro03 <tato11901@gmail.com>
2026-08-14 20:59:21 -0500

    Movement and camera fixes

--- Assets/Scripts/VirtualPad.cs
+++ Assets/Scripts/VirtualPad.cs
@@ -1,114 +1,82 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Rendering;
-using UnityEngine.UI;
 using UnityEngine.InputSystem.EnhancedTouch;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
-using UnityEditor.Experimental.GraphView;
 
-
+[RequireComponent(typeof(PlayerInput))]
 public class VirtualPad : MonoBehaviour
 {
     public GameObject character;
     public float speed = 0.5f;
     public Camera camara;
-    public float cameraSpeed = 1f;
+    public float cameraSpeed = 0.1f;
+    public float pitchClamp = 85f;
 
     private PlayerInput playerInput;
     private InputAction moveAction;
-    private InputAction cameraAction;
     private Vector2 moveInput;
-    private Vector2 cameraInput;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float pitch;
+    private float yaw;
+
+    void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
+    }
+
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
-        Debug.Log("PlayerInput: " + playerInput);
-
-        moveAction = playerInput.actions["Move"];
-        Debug.Log("Move Action: " + moveAction);
-
-        cameraAction = playerInput.actions["Camera"];
-        Debug.Log("Camera Action " + cameraAction);
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        var currentTouches = Touch.activeTouches;
-
-        foreach(var touch in currentTouches)
+        if (playerInput == null || playerInput.actions == null)
         {
-           if (touch.startScreenPosition.x < Screen.width / 2f)
-           continue; 
-           Vector2 direction = calculateDirection(touch.startScreenPosition, touch.screenPosition);
-           MoveCamera(direction.x, direction.y);
+            Debug.LogError("VirtualPad: falta un PlayerInput con una InputActionAsset asignada.");
+            return;
         }
 
-        MoveCharacter();
-
+        moveAction = playerInput.actions["Move"];
+        if (moveAction == null)
+            Debug.LogError("VirtualPad: no existe la accion 'Move' en la InputActionAsset.");
     }
 
+    void Update()
+    {
+        if (character == null || camara == null)
+            return;
 
-    public void MoveCharacter()
+        RotateCameraFromTouch();
+        MoveCharacter();
+    }
+
+    void MoveCharacter()
     {
         moveInput = moveAction.ReadValue<Vector2>();
-
-        //Debug.Log("Move Input: " + moveInput);
-
 
         if (moveInput.sqrMagnitude > 0.01f)
         {
             moveInput = moveInput.normalized;
+            Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y);
+            character.transform.Translate(movement * speed * Time.deltaTime, Space.World);
         }
-
-        else
-        {
-            moveInput = Vector2.zero;
-        }
-
-        Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y);
-        character.transform.Translate(movement * speed * Time.deltaTime);
-        //print(character.transform.position);
     }
 
-    public void MoveCamera(float x, float y)
+    void RotateCameraFromTouch()
     {
-        cameraInput = cameraAction.ReadValue<Vector2>();
-
-        
-        if( x > 0)
+        foreach (var touch in Touch.activeTouches)
         {
-            camara.transform.Rotate(speed * Time.deltaTime, 0f, 0f);
+            if (touch.screenPosition.x < Screen.width / 2f)
+                continue;
+
+            Vector2 delta = touch.delta;
+            yaw += delta.x * cameraSpeed;
+            pitch -= delta.y * cameraSpeed;
         }
 
-        if( x < 0)
-        {
-            camara.transform.Rotate(-speed * Time.deltaTime, 0f, 0f);
-        }
-
-        if( x > 0)
-        {
-            camara.transform.Rotate(0f, speed * Time.deltaTime, 0f);
-        }
-
-        if( x < 0)
-        {
-            camara.transform.Rotate(0f, -speed * Time.deltaTime, 0f);
-        }
-
-        
-
-        
-    }
-
-    public Vector2 calculateDirection(Vector2 StartPos, Vector2 currentPos)
-    {
-        Vector2 direction = currentPos - StartPos;
-        direction = direction.normalized;
-        print(direction);
-        return direction;
+        pitch = Mathf.Clamp(pitch, -pitchClamp, pitchClamp);
+        camara.transform.rotation = Quaternion.Euler(pitch, yaw, 0f);
     }
 }
